eac87d8f9207a0326cdfdfad666914a7895e1bd6
abdulrahmen-code <abdulrahmenmashhadi@gmail.com>
2026-07-24 16:24:23 +0200

    Add files via upload

---
+++ seconds perfekt.py
@@ -0,0 +1,2 @@
+int_seconds = int(input ("Please type the number of seconds: \n"))
+print (f"The course is {int_seconds // 3600} hours and {int_seconds % 3600 // 60} minutes and {int_seconds % 60}")
